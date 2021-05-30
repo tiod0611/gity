4aad6ecd628e39a2d00dfbc55a3ad8ecccc29317
JeongMinHyeok <tlsfk48@gmail.com>
2021-05-30 10:50:11 +0900

    repush segmentation by rgb

---
+++ To_do_list.md
@@ -0,0 +1,45 @@
+# 공통
++ 코드에 기능별 주석 달아줄 것
++ 실행에 필요한 정보들 README에 기록할 것
+
+# 진행 중
+## kyeul 
++ 
+## minding
++ 
+
+## jerry
++ 
+
+# 할  일 
+## kyeul
+1. 저장 형식 세분화 cut image & get coordiante
+2. Linux용 크롤링 & download video
+3.
+
+
+## minding
+1. 정사각형으로 이미지 자르기
+2. 
+3. 
+
+## jerry
+1. GPU 자원 사용할 수 있게 할 것!
+2. 동영상으로 어디를 검출하는지 볼 수 있게 할 것
+3. 본래의 size(1280,720)로 좌표값 추출되게 기능 추가
+
+
+# 완료(완성된 기능은 아래에 기록해주세요.)
+## Kyeul
++ 크롤링 (windows10)
++ 비디오1 다운로드 (windows10)
++
+
+## minding
++ 좌표 받아 이미지 자르기
++ 객체 별 분류(RGB value (R, G, B) 각각 사용)
++ 이미지 저장
+
+## jerry
++ 객체 검출 기능 구현 (yolov3)
++
---
+++ video_preprocessing.py
@@ -0,0 +1,83 @@
+import numpy as np
+import imageio
+import os
+import warnings
+from detect_test import detection
+from argparse import ArgumentParser
+from skimage import img_as_ubyte
+from skimage.transform import resize
+warnings.filterwarnings("ignore")
+
+DEVNULL = open(os.devnull, 'wb')
+
+# 객체 번호별 이미지 저장 (Images save by object number)
+def save(path, frames, seq, obj_num):
+    if os.path.exists(path):
+        imageio.imsave(os.path.join(path, str(seq).zfill(7) + '#' + str(obj_num).zfill(1) + '.png'),frames)
+    else:
+        os.makedirs(path)
+    imageio.imsave(os.path.join(path, str(seq).zfill(7) + '#' + str(obj_num).zfill(1) + '.png'), frames)
+
+
+# RGB합계값으로 객체 세분화 및 번호 부여 (Object segmentation by RGB value)
+def obj_segmentation(crop_rgb_dict, crop_list):
+	obj_num = 0
+	if crop_rgb_dict == {}:
+		crop_rgb_dict[1] = crop_list
+		obj_num = 1
+	else:
+		for key, value in crop_rgb_dict.items():
+			# 기준객체의 RGB값 각각 +- 20% 값으로 동일객체 판단
+			pass_crop = 0
+			for i, rgb in enumerate(value):
+				if crop_list[i] <= (rgb * 1.2) and crop_list[i] >= (rgb * 0.8):
+					pass_crop += 1
+					if pass_crop == 3: # r,g,b 모두 조건에 맞다면 해당 key_number 부여
+						obj_num = key
+						break
+				else :
+					break
+					
+		if obj_num == 0:
+			crop_rgb_dict[max(crop_rgb_dict.keys()) + 1] = crop_list
+			obj_num = max(crop_rgb_dict.keys())
+
+	return crop_rgb_dict, obj_num
+
+
+# 비디오 불러오기, 좌표값을 받아 이미지 자르기 (Load video, Crop images by coordinate value)
+# def run(data):
+def run(download, acc, image_shape, out_folder, video_id, class_name):
+    # video_id, args = data
+	video_path = os.path.join(download, os.listdir(download)[0])
+	reader = imageio.get_reader(video_path)
+	crop_rgb_dict = {}
+
+	try:
+		for i, frame in enumerate(reader):
+			# import detection coordinate (bbox location = (x1, x2, y1, y2))
+			# resize to detection model input size (416, 416)
+			if i % 5 == 0:
+				frame = img_as_ubyte(resize(frame, (416,416), anti_aliasing=True))
+				bboxes = detection(frame, acc, class_name) # 프레임 데이터와 검출정확도
+				print(i, bboxes) # 프레임 별 좌표 검출 정보
+			else:
+				continue
+
+			for bbox in bboxes:
+				x1, x2, y1, y2 = bbox
+				crop = frame[y1:y2, x1:x2]
+				# 잘라낸 이미지의 RGB값 합계
+				crop_list = [crop[:, :, 0].sum(), crop[:, :, 1].sum(), crop[:, :, 2].sum()]
+				crop_rgb_dict, obj_num = obj_segmentation(crop_rgb_dict, crop_list)
+				
+				# 입력받은 이미지 크기(shape)로 재조정
+				if image_shape is not None:
+					crop = img_as_ubyte(resize(crop, image_shape, anti_aliasing=True))
+				# 이미지 저장 경로 설정
+				first_part = ""
+				first_part += '#' + video_id
+				path = first_part + '.mp4' + '#' + str(obj_num)
+				save(os.path.join(out_folder, path), crop, i, obj_num)
+	except imageio.core.format.CannotReadFrameError:
+		None
