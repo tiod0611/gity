2c52847b637584702b65de71aed9f4f2b4d6076e
kyeul611 <gyul611@likelion.org>
2021-10-16 00:59:13 +0900

    update code

---
+++ README.md
@@ -0,0 +1,97 @@
+# GITY
+Gather Image Through Youtube
+---
+유튜브에서 원하는 객체를 검출하여 이미지로 저장해주는 코드입니다.  
+
+<img src="./information/image1.png">
+
+
+
+## 목차
+
+- [환경](#environment)
+- [설치](#installation)
+- [실행](#run)
+- [작성자](#author)
+## Environment
++ OS  
+  __ubuntu 20.04__ 에서 테스트를 진행했습니다.
++  GPU  
+  __RTX3090__ 에서 테스트를 진행했습니다.
++ Tensorflow  
+  __tensorflow 2.0__ 이상 에서 작동합니다.
+
+
+
+## Installation
+0. clone repogitory
+```
+git clone https://github.com/kyeul611/gity.git
+cd ./gity
+```
+1. 모듈 설치
+```
+pip install -r requirements.txt
+```
+
+2. chromedrive
+```
+apt-get update
+apt install chromium-chromedriver
+```
+3. youtube-dl
+```
+wget https://yt-dl.org/downloads/latest/youtube-dl -O youtube-dl
+chmod a+rx youtube-dl
+```
+
+#### Detector
+1. detection-weight
+
+   [이곳](https://drive.google.com/drive/folders/1zB0tJ1U7zNmxUGGbpSF9RhG2szbK1m1r)에서 __yolov3.tf.data-00000-of-00001__ 파일을 다운받아 __gity/yolov3_tf2__ 폴더 안으로 옮겨주세요.
+
+
+
+## run
+
+터미널에서 아래 명령어를 실행
+```
+python main.py --keyword [키워드] --class_name [클래스 종류] 
+```
+
+  ___Detection Class___
+   Object-detector는 COCO Dataset의 에 대해서 작동합니다.  
+   탐지 가능한 클래스 종류는 [이곳](./information/class_list.txt)에서 확인 가능합니다.
+
+example:
+
+```
+python main.py --keyword tusker\ elephant --class_name elephant --num_video 5 
+```
+
+추가 인자는 아래 명령어를 통해 확인하세요.
+```
+python main.py --help
+```
+
+
+
+## references
+
+다음 Open source를 참고하였습니다.
+
++ [video-preprocessing](https://github.com/AliaksandrSiarohin/video-preprocessing)
+
++ [object detector](https://github.com/zzh8829/yolov3-tf2)
+
+
+
+## author
+
+```python
+{
+	"박 결" : "gyul611@gmail.com",
+	"정민혁" : "tlsfk48@gmail.com",
+	"이제헌" : "dlwpgjs0723@gmail.com",
+}
+```
---
+++ crawler.py
@@ -0,0 +1,61 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import os
+import time
+import re
+import platform
+
+def crawling(keyword, numberOfvideos):
+    print("\n>> Let's crawling!<< \n")
+    keyword = keyword
+    numberOfvideos = numberOfvideos
+    urls = []
+
+   
+
+
+    chrome_options = webdriver.ChromeOptions()
+    chrome_options.add_argument('--headless')
+    chrome_options.add_argument('--no-sandbox')
+    chrome_options.add_argument('--disable-dev-shm-usage')
+    if platform.system()=='Windows':
+        
+        driver = webdriver.Chrome(os.path.join(os.getcwd(), 'chromedriver.exe'),chrome_options=chrome_options)
+    elif platform.system()=='Linux':
+        driver = webdriver.Chrome('chromedriver', chrome_options=chrome_options)
+
+    address = [
+        'https://www.youtube.com/results?search_query={}&sp=EgQQARgD',
+        'https://www.youtube.com/results?search_query={}&sp=EgQQARgB'
+    ]
+
+    for i in address:
+        driver.get(i.format(keyword))
+        # WebDriverWait(driver, 5).until(EC.element_to_be_clickable((By.CSS_SELECTOR, "input#search"))).send_keys("야구공")
+        # driver.find_element_by_css_selector("button.style-scope.ytd-searchbox#search-icon-legacy").click()
+
+        # while True:
+        for i in range(numberOfvideos):
+            scroll_height = 1000
+            document_height_before = driver.execute_script("return document.documentElement.scrollHeight")
+            driver.execute_script(f"window.scrollTo(0, {document_height_before + scroll_height});")
+            time.sleep(1.3)
+            document_height_after = driver.execute_script("return document.documentElement.scrollHeight")
+            if document_height_after == document_height_before:
+                break
+
+        for my_href in WebDriverWait(driver, 5).until(EC.visibility_of_all_elements_located((By.CSS_SELECTOR, "a.yt-simple-endpoint.style-scope.ytd-video-renderer#video-title"))):
+            urls.append(re.sub("https://www\.youtube\.com/watch\?v=", "", my_href.get_attribute("href")))
+    
+    driver.close()
+    print("총 영상 수: {}".format(len(urls)))
+    return urls
+
+
+
+
+
+
+
